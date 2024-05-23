ba47fdfae22ea8824c7e0aee4ccb94c61e000a7a
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-05-23 14:39:47 -0300

    Add files via upload

---
+++ projetoSomativa/src/main/java/com/example/demo/ProjetoSomativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSomativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSomativaApplication.class, args);
+	}
+
+}
